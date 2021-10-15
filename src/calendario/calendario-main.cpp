52fe75b2411f594e14c12838b9a5db5f7b0fb504
Miguel Ángel Latre Abadía <latre@unizar.es>
2021-10-15 19:17:46 +0200

    Cabecera (tema 7 --> práctica 3)

--- src/calendario/calendario-main.cpp
+++ src/calendario/calendario-main.cpp
@@ -1,14 +1,14 @@
-/*********************************************************************************
- * Curso de Programación 1. Tema 7
+/*******************************************************************************
+ * Curso de Programación 1. Práctica 3
  * Autores: ¡PON AQUÍ TU NOMBRE!
  * Última revisión: 23 de octubre de 2019
  * Resumen: Programa que escribe en la pantalla el calendario de un determinado
- *          mes de un año, que se solicitan al usuario. El programa se asegura de
- *          que el mes introducido está comprendido entre 1 y 12 y de que el año
- *          es igual o posterior a 1900.
+ *          mes de un año, que se solicitan al usuario. El programa se asegura
+ *          de que el mes introducido está comprendido entre 1 y 12 y de que el
+ *          año es igual o posterior a 1900.
  *          Clase de problemas correspondiente al tema 7 (diseño modular y
  *          descendente) y 3ª práctica.
-\********************************************************************************/
+\******************************************************************************/
 
 
 /*
